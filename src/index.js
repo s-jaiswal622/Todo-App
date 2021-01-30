88e1319039ade93d8b896f1bf5657648d7643a31
saloni <saloni.jaiswal@letstransport.team>
2021-01-30 10:17:48 +0530

    Todo App

--- src/index.js
+++ src/index.js
@@ -1,17 +1,71 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+class App1 extends React.Component {
+  constructor(props)
+  {
+    super(props);
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    this.state = {
+      newItem: "",
+      list: [],
+      count:0
+    };
+    this.addItem = this.addItem.bind(this);
+    this.updateInput = this.updateInput.bind(this);
+    //this.deleteItem = this.deleteItem.bind(this);
+  }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  updateInput (event)
+  {
+    this.setState({newItem: event.target.value});
+  }
+
+  addItem()
+  {
+    const todo= {
+     id: this.state.count,
+      text: this.state.newItem
+    };
+
+    let list = this.state.list;
+    list.push(todo);
+    this.setState(
+    {list: list,
+    newItem: "",
+    count: this.state.count + 1
+    });
+  }
+
+  deleteItem(id)
+  {
+   let list = this.state.list;
+   let updatedList = list.filter(item => item.id!=id);
+    this.setState({ list: updatedList });
+  }
+
+  render()
+  {
+    return(
+      <div className="App">
+        <div>Add an Item</div>
+        <br />
+        <input type="text" placeholder="type the item here" value= {this.state.newItem}
+        onChange={this.updateInput}/>
+        <button onClick = {this.addItem} >Add</button>
+        <br/>
+          {this.state.list.map((arrItem) =>
+            {
+              return(
+                <li key={arrItem.id}>
+                {arrItem.text}
+                <button onClick = {() => this.deleteItem(arrItem.id)}> X </button>
+                </li>
+              );
+            }
+          )}
+      </div>
+    );
+  }
+}
+ReactDOM.render(<App1/>, document.getElementById("root"));
+export default App1;
