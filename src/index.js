342c226678c7f990acf1390d4f1ff449a6a531e5
saloni <saloni.jaiswal@letstransport.team>
2021-01-30 10:43:13 +0530

    Todo-App with functional component

--- src/index.js
+++ src/index.js
@@ -1,71 +1,42 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom";
-class App1 extends React.Component {
-  constructor(props)
+
+function App () {
+const [newItem, setNewItem] = useState('');
+const [list, setList] = useState([]);
+const [count, setCount] = useState(0);
+
+const addItem = () => 
   {
-    super(props);
-
-    this.state = {
-      newItem: "",
-      list: [],
-      count:0
-    };
-    this.addItem = this.addItem.bind(this);
-    this.updateInput = this.updateInput.bind(this);
-    //this.deleteItem = this.deleteItem.bind(this);
-  }
-
-  updateInput (event)
-  {
-    this.setState({newItem: event.target.value});
-  }
-
-  addItem()
-  {
-    const todo= {
-     id: this.state.count,
-      text: this.state.newItem
-    };
-
-    let list = this.state.list;
+    const todo= {id: count, text: newItem };
     list.push(todo);
-    this.setState(
-    {list: list,
-    newItem: "",
-    count: this.state.count + 1
-    });
+    setList(list);
+    setNewItem('');
+    setCount(count + 1);
   }
 
-  deleteItem(id)
+  const deleteItem = (id) =>
   {
-   let list = this.state.list;
    let updatedList = list.filter(item => item.id!=id);
-    this.setState({ list: updatedList });
+    setList(updatedList);
   }
-
-  render()
-  {
-    return(
-      <div className="App">
-        <div>Add an Item</div>
-        <br />
-        <input type="text" placeholder="type the item here" value= {this.state.newItem}
-        onChange={this.updateInput}/>
-        <button onClick = {this.addItem} >Add</button>
-        <br/>
-          {this.state.list.map((arrItem) =>
-            {
-              return(
-                <li key={arrItem.id}>
-                {arrItem.text}
-                <button onClick = {() => this.deleteItem(arrItem.id)}> X </button>
-                </li>
-              );
-            }
-          )}
-      </div>
-    );
-  }
+  return(
+    <div className="App">
+      <div>Add an Item</div>
+      <br />
+      <input type="text" placeholder="type the item here" value= {newItem} onChange={ e => setNewItem(e.target.value) }/>
+      <button onClick = {addItem} >Add</button>
+      <br/>
+        {list.map((arrItem) => {
+            return(
+              <li key={arrItem.id}>
+              {arrItem.text}
+              <button onClick = {() => deleteItem(arrItem.id)}> X </button>
+              </li>
+            );
+          }
+        )}
+    </div>
+  );
 }
-ReactDOM.render(<App1/>, document.getElementById("root"));
-export default App1;
+ReactDOM.render(<App/>, document.getElementById("root"));
